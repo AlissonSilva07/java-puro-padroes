4d049d07dcd04e4d03d37424f79f4ef7dcab2920
Alisson Rego <alissondroid07@gmail.com>
2023-10-05 08:57:54 -0300

    Primeira implementação: preguiçoso

---
+++ src/edu/alisson/singleton/SingletonLazy.java
@@ -0,0 +1,20 @@
+package edu.alisson.singleton;
+
+/**
+ * Singleton preguiçoso
+ * @author AlissonSilva07
+ */
+public class SingletonLazy {
+    private static SingletonLazy instancia;
+
+    private SingletonLazy () {
+        super();
+    }
+
+    public static SingletonLazy getInstancia() {
+        if (instancia == null) {
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
+}
